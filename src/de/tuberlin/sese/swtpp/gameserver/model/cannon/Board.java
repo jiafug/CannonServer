47d71c7009681708d7e3e28cfcce9814dbedf7a6
jiafug <comet.liu@protonmail.com>
2018-02-01 21:24:21 +0100

    Update Board.java

--- src/de/tuberlin/sese/swtpp/gameserver/model/cannon/Board.java
+++ src/de/tuberlin/sese/swtpp/gameserver/model/cannon/Board.java
@@ -1,6 +1,7 @@
 package de.tuberlin.sese.swtpp.gameserver.model.cannon;
 
 import java.io.Serializable;
+import java.util.HashSet;
 
 public class Board implements Serializable {
 
@@ -12,9 +13,12 @@ public class Board implements Serializable {
 	private int[][] board = new int[10][10];
 	private CannonGame game;
 	private final String letterIndex = "abcdefghij";
+	private HashSet<String> possibleMoves;
 
 	public Board(CannonGame game) {
 		this.game = game;
+		this.possibleMoves = new HashSet<>();
+
 	}
 
 	// [0][0] ist das Feld oben links, also a9, [9][9] ist j0 um mit dem
@@ -30,13 +34,13 @@ public class Board implements Serializable {
 			for (int j = 0; j < 10; j++) {
 				if (board[i][j] == 0)
 					tmp = tmp + "1";
-				else if (board[i][j] == 1)
+				if (board[i][j] == 1)
 					tmp = tmp + "w";
-				else if (board[i][j] == 2)
+				if (board[i][j] == 2)
 					tmp = tmp + "W";
-				else if (board[i][j] == -1)
+				if (board[i][j] == -1)
 					tmp = tmp + "b";
-				else if (board[i][j] == -2)
+				if (board[i][j] == -2)
 					tmp = tmp + "B";
 			}
 			if (tmp.contentEquals("1111111111"))
@@ -79,6 +83,7 @@ public class Board implements Serializable {
 	}
 
 	public void setBoard(String fen) {
+		board = new int[10][10];
 		int iCount = 0;
 		int jCount = 0;
 		for (int f = 0; f < fen.length(); f++) {
@@ -92,34 +97,34 @@ public class Board implements Serializable {
 				iCount++;
 			}
 			// eine Reihe weiter am Ende
-			else if (c == '/' && jCount != 0) {
+			if (c == '/' && jCount != 0) {
 				iCount++;
 				jCount = 0;
 			}
 			// Null Feld
-			else if (space > 0 && space < 10) {
+			if (space > 0 && space < 10) {
 				for (int i = 0; i < space; i++) {
 					board[iCount][jCount] = 0;
 					jCount++;
 				}
 			}
 			// Schwarz Soldat
-			else if (c == 'b') {
+			if (c == 'b') {
 				board[iCount][jCount] = -1;
 				jCount++;
 			}
 			// Schwarz Stadt
-			else if (c == 'B') {
+			if (c == 'B') {
 				board[iCount][jCount] = -2;
 				jCount++;
 			}
 			// Weiss Soldat
-			else if (c == 'w') {
+			if (c == 'w') {
 				board[iCount][jCount] = 1;
 				jCount++;
 			}
 			// Weiss Stadt
-			else if (c == 'W') {
+			if (c == 'W') {
 				board[iCount][jCount] = 2;
 				jCount++;
 			}
@@ -143,22 +148,72 @@ public class Board implements Serializable {
 	 */
 	public boolean performMove(String move) {
 		// Berechnet die Position im board-Array
+		if (this.possibleMoves.contains(move)) {
+			this.possibleMoves.clear();
+			int startX = letterIndex.indexOf(move.charAt(0));
+			int startY = Math.abs(Character.getNumericValue(move.charAt(1)) - 9);
+			int destX = letterIndex.indexOf(move.charAt(3));
+			int destY = Math.abs(Character.getNumericValue(move.charAt(4)) - 9);
+			// Ueberprueft ob die zu bewegene Figur die richtige Farbe hat und
+			// welche Zuege moeglich sind
+			if (cannonAttack(startX, startY, destX, destY)) {
+				board[destY][destX] = 0;
+				return true;
+			} else if (game.isWhiteNext()) {
+				board[startY][startX] = 0;
+				board[destY][destX] = 1;
+				return true;
+			} else {
+				board[startY][startX] = 0;
+				board[destY][destX] = -1;
+				return true;
+			}
+		} else
+			return false;
+	}
+
+	/**
+	 * finds and saves all possible moves in a hash map
+	 */
+	public HashSet<String> allPossibleMoves() {
+		String move = "";
+		for (char c : letterIndex.toCharArray()) {
+			for (int i = 0; i < 10; i++) {
+				move = String.valueOf(c) + i + "-";
+				allPossibleMovesHelper(move);
+				move = "";
+			}
+		}
+		return this.possibleMoves;
+	}
+
+	/**
+	 * method to reduce the nested block depth of anyMovePossible()
+	 */
+	private void allPossibleMovesHelper(String move) {
+		for (char c : letterIndex.toCharArray()) {
+			for (int i = 0; i < 10; i++) {
+				move += String.valueOf(c) + i;
+				if (checkMove(move))
+					possibleMoves.add(move);
+				move = move.substring(0, 3);
+			}
+		}
+	}
+
+	/**
+	 * checks weather a move is possible
+	 */
+	private boolean checkMove(String move) {
 		int startX = letterIndex.indexOf(move.charAt(0));
 		int startY = Math.abs(Character.getNumericValue(move.charAt(1)) - 9);
 		int destX = letterIndex.indexOf(move.charAt(3));
 		int destY = Math.abs(Character.getNumericValue(move.charAt(4)) - 9);
-		// Ueberprueft ob die zu bewegene Figur die richtige Farbe hat und
-		// welche Zuege moeglich sind
 		if (checkWhite(startX, startY, destX, destY)) {
-			board[startY][startX] = 0;
-			board[destY][destX] = 1;
 			return true;
 		} else if (checkBlack(startX, startY, destX, destY)) {
-			board[startY][startX] = 0;
-			board[destY][destX] = -1;
 			return true;
 		} else if (cannonAttack(startX, startY, destX, destY)) {
-			board[destY][destX] = 0;
 			return true;
 		} else
 			return false;
@@ -168,21 +223,21 @@ public class Board implements Serializable {
 	 * method to reduce the cyclomatic complexity of performMove(String move)
 	 */
 	private boolean checkWhite(int startX, int startY, int destX, int destY) {
-		return (game.isWhiteNext() && (board[startY][startX] == 1) && (simpleMove(startX, startY, destX, destY)
-				|| attackMove(startX, startY, destX, destY) || retreatMove(startX, startY, destX, destY) 
-				|| cannonMove(startX, startY, destX, destY)));
+		return (game.isWhiteNext() && (board[startY][startX] == 1)
+				&& (simpleMove(startX, startY, destX, destY) || attackMove(startX, startY, destX, destY)
+						|| retreatMove(startX, startY, destX, destY) || cannonMove(startX, startY, destX, destY)));
 	}
 
 	/**
 	 * method to reduce the cyclomatic complexity of performMove(String move)
 	 */
 	private boolean checkBlack(int startX, int startY, int destX, int destY) {
-		return (!game.isWhiteNext() && (board[startY][startX] == -1) && (simpleMove(startX, startY, destX, destY)
-				|| attackMove(startX, startY, destX, destY) || retreatMove(startX, startY, destX, destY) 
-				|| cannonMove(startX, startY, destX, destY)));
+		return (!game.isWhiteNext() && (board[startY][startX] == -1)
+				&& (simpleMove(startX, startY, destX, destY) || attackMove(startX, startY, destX, destY)
+						|| retreatMove(startX, startY, destX, destY) || cannonMove(startX, startY, destX, destY)));
 	}
 
-/**
+	/**
 	 * checks if a simple move is possible
 	 */
 	private boolean simpleMove(int startX, int startY, int destX, int destY) {
@@ -209,8 +264,9 @@ public class Board implements Serializable {
 			var = -1;
 		else
 			var = 1;
-		return (destX == startX - 1 || destX == startX + 1) && (destY == startY)
-				&& (board[destY][destX] == var || board[destY][destX] == var * 2);
+		if (board[destY][destX] == var * 2)
+			var = var * 2;
+		return (destX == startX - 1 || destX == startX + 1) && (destY == startY) && (board[destY][destX] == var);
 	}
 
 	/**
@@ -220,18 +276,24 @@ public class Board implements Serializable {
 		// Ueberprueft ob das Zielfeld leer ist und keine Figur im Weg steht und
 		// ob der Schritt genau zwei Felder nach hinten gerade oder diagonal
 		// verlaeuft
-		int var;
-		if (game.isWhiteNext())
-			var = -1;
-		else
-			var = 1;
-		return (board[destY][destX] == 0
-				&& (destY == startY + (2 * var) && (board[startY + 1][startX] == var || board[startY][startX - 1] == var
-						|| board[startY + 1][startX - 1] == var || board[startY][startX + 1] == var
-						|| board[startY + 1][startX + 1] == var || board[startY - 1][startX] == var
-						|| board[startY - 1][startX - 1] == var || board[startY - 1][startX + 1] == var))
-				&& (destX == startX - 2 || destX == startX + 2 || destX == startX)
-				&& board[(destY + startY) / 2][(destX + startX) / 2] == 0);
+		try {
+			int var;
+			if (game.isWhiteNext())
+				var = -1;
+			else
+				var = 1;
+			return (board[destY][destX] == 0
+					&& (destY == startY + (2 * var)
+							&& (board[startY + 1][startX] == var || board[startY][startX - 1] == var
+									|| board[startY + 1][startX - 1] == var || board[startY][startX + 1] == var
+									|| board[startY + 1][startX + 1] == var || board[startY - 1][startX] == var
+									|| board[startY - 1][startX - 1] == var || board[startY - 1][startX + 1] == var))
+					&& (destX == startX - 2 || destX == startX + 2 || destX == startX)
+					&& board[(destY + startY) / 2][(destX + startX) / 2] == 0);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+
 	}
 
 	/**
@@ -262,7 +324,6 @@ public class Board implements Serializable {
 						&& board[startY][startX - 2] == var
 				|| (destY - startY == 3) && (destX - startX == -3) && board[startY + 1][startX - 1] == var
 						&& board[startY + 2][startX - 2] == var)));
-
 	}
 
 	/**
